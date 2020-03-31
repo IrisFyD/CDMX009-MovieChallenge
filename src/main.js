2e3c64422b27bc08a606ef2a6866ef6cb95cdd9e
IrisFyD <iriis.trejo.c@gmail.com>
2020-03-31 17:03:53 -0600

    Delete data,for each ahd entries

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //References
 let nameItem = document.getElementById('nameItem');
+let container = document.getElementById('root');
 
 // Observers
 let buttonSendItem = document.getElementById('button');
@@ -8,11 +9,15 @@ buttonSendItem.addEventListener('click', getResult);
 //Functions
 function getResult(){
     let nameItemReference = nameItem.value;
-    fetch('http://www.omdbapi.com/?t='+ nameItemReference + '&apikey=3d2c4f6f')
+    fetch('http://www.omdbapi.com/?s='+ nameItemReference + '&apikey=3d2c4f6f')
     .then(res => res.json())
     .then(data => {
-       const moviesName = data;
-        console.log(moviesName);
+        //creates a const that saves the results
+       const moviesName = data.Search;
+       //iterates through each element in moviesName
+       moviesName.forEach(element => {
+           //Save the content in the  let container
+           container.innerHTML += Object.entries(element);
+       });
     })
-}
-
+};
