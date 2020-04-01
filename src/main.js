afcafa904a1f3da002b6ab58e0e5e9a08533c54c
IrisFyD <iriis.trejo.c@gmail.com>
2020-04-01 01:37:15 -0600

    slider, button search, icons, card

--- src/main.js
+++ src/main.js
@@ -1,13 +1,50 @@
+//Fuction slider
+let index = 1;
+showSlides(index);
+
+function nextSlide(){
+    showSlides(index+=n);
+}
+
+function positionSlide(n){
+    showSlides(index=n);
+}
+
+setInterval(function time(){
+    showSlides(index+=1)
+},4000);
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName('ourSlider');
+    let bars = document.getElementsByClassName('bar');
+
+    if (n > slides.length){
+        index = 1;
+    }
+    if(n < 1){
+        index = slides.length;
+    }
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for (i = 0; i < bars.length; i++){
+        bars[i].className = bars[i].className.replace('active', '');
+    }
+    slides[index-1].style.display = 'block';
+    bars[index-1].className += ' active';
+}
+
 //References
 let nameItem = document.getElementById('nameItem');
 let container = document.getElementById('root');
 
 // Observers
-let buttonSendItem = document.getElementById('button');
-buttonSendItem.addEventListener('click', getResult);
+let buttonSendItem = document.getElementById('nameButton');
+buttonSendItem.addEventListener('click', getResultName);
 
 //Functions
-function getResult(){
+function getResultName(){
     let nameItemReference = nameItem.value;
     fetch('http://www.omdbapi.com/?s='+ nameItemReference + '&apikey=3d2c4f6f')
     .then(res => res.json())
@@ -17,7 +54,12 @@ function getResult(){
        //iterates through each element in moviesName
        moviesName.forEach(element => {
            //Save the content in the  let container
-           container.innerHTML += Object.entries(element);
+           const paraph= document.createElement('p')
+           paraph.classList.add('card')
+           paraph.innerHTML += `
+           <a href='#'><img src=${element.Poster}/></a>
+           `
+           container.appendChild(paraph)
        });
     })
 };
